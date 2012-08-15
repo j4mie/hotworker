bb217be936ea3f204ef72aaab9e9b45273f6a285
Jamie Matthews <jamie.matthews@gmail.com>
2012-08-15 18:19:37 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hotworker',
-    version='0.1.0',
+    version='0.2.0',
     description='Unix-friendly HotQueue worker processes',
     author='Jamie Matthews',
     author_email='jamie.matthews@gmail.com',
